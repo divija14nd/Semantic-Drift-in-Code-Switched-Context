bb013be414b7bbd3a66a0e556e3b72e178b96cc3
ohitssammy <sammytruong2@gmail.com>
2025-10-19 17:21:33 -0600

    Include more emoji unicodes

--- src/data_preparation/preprocess_data.py
+++ src/data_preparation/preprocess_data.py
@@ -8,15 +8,26 @@ SPACE_REGEX = re.compile(r'\s+')
 MENTION_REGEX = re.compile(r'@\w+')
 HASHTAG_REGEX = re.compile(r'#\w+')
 EMOJI_REGEX   = re.compile(
-    "["                       
-    "\U0001F600-\U0001F64F"   
-    "\U0001F300-\U0001F5FF"   
-    "\U0001F680-\U0001F6FF"   
-    "\U0001F1E0-\U0001F1FF"   
-    "\U00002700-\U000027BF"   
-    "\U000024C2-\U0001F251"   
+    "["
+    "\U0001F1E0-\U0001F1FF"
+    "\U0001F300-\U0001F5FF"
+    "\U0001F600-\U0001F64F"
+    "\U0001F680-\U0001F6FF"
+    "\U0001F700-\U0001F77F"
+    "\U0001F780-\U0001F7FF"
+    "\U0001F800-\U0001F8FF"
+    "\U0001F900-\U0001F9FF"
+    "\U0001FA00-\U0001FA6F"
+    "\U0001FA70-\U0001FAFF"
+    "\U00002700-\U000027BF"
+    "\U00002600-\U000026FF"
+    "\U00002B00-\U00002BFF"
+    "\U00002300-\U000023FF"
+    "\U000025A0-\U000025FF"
+    "\U000024C2-\U0001F251"
     "]+", flags=re.UNICODE
 )
+
 
 def clean_text(text: str):
     if not isinstance(text, str):
